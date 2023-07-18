31e89eca6af5cf47b922d1a7b154eb81510b842c
Gord <gord@ghdunker.ca>
2023-07-17 20:54:35 -0400

    Changed quoting

--- planet_hour2.sh
+++ planet_hour2.sh
@@ -18,49 +18,49 @@ declare -a hplanets_i=(0 3 6 2 5 1 4)
 # TfS = Time from set 
 # DRS = Interval between Rise and Set 
 
-R=`getRisetime`
-S=`getSettime`
+R=$(getRisetime)
+S=$(getSettime)
 
 
 cur_l="" # Left part of current time indicator
 cur_r="" # Right '' ''
 
 # Must use full date with Z when adding / subtracting time
-Rh=`date --date="$R" +%H`
-Rm=`date --date="$R" +%M`
-DR=`date --date="$R" +%Y-%m-%d\ %R\ %Z`
-DS=`date --date="$S" +%Y-%m-%d\ %R\ %Z`
-DRS=`date --date="$DS -$Rh hours -$Rm minutes" +%H:%M`
-DRS_h=`date --date="$DRS" +%H`
+Rh=$(date --date="$R" +%H)
+Rm=$(date --date="$R" +%M)
+DR=$(date --date="$R" +%Y-%m-%d\ %R\ %Z)
+DS=$(date --date="$S" +%Y-%m-%d\ %R\ %Z)
+DRS=$(date --date="$DS -$Rh hours -$Rm minutes" +%H:%M)
+DRS_h=$(date --date="$DRS" +%H)
 
-tn=`date +%Y-%m-%d\ %R\ %Z`
-tnh=`date --date="$tn" +%H`
-tnm=`date --date="$tn" +%M`
+tn=$(date +%Y-%m-%d\ %R\ %Z)
+tnh=$(date --date="$tn" +%H)
+tnm=$(date --date="$tn" +%M)
 
 # if $tnh:$tnm is greater than 00:00 and < $Rh:$Rm
 # then the date is the previous day 
-# tn=`date --date="$tn -1 day" +%Y-%m-%d\ %R\ %Z`
+# tn=$(date --date="$tn -1 day" +%Y-%m-%d\ %R\ %Z)
 # 
-if [[ `comp_time $tnh:$tnm 00:00` -eq 1 && `comp_time $tnh:$tnm $Rh:$Rm` -eq 0  ]];
+if [[ `comp_time $tnh:$tnm 00:00` -eq 1 && $(comp_time $tnh:$tnm $Rh:$Rm) -eq 0  ]];
 then
-  tn=`date --date="$tn -1 day" +%Y-%m-%d\ %R\ %Z`
+  tn=$(date --date="$tn -1 day" +%Y-%m-%d\ %R\ %Z)
   echo "Rise Date: $tn"
-  day_of_week=`date --date="$tn" +%w`
+  day_of_week=$(date --date="$tn" +%w)
   echo "day_of_week: $day_of_week"
   echo "late but early"
 else
-  day_of_week=`date --date="$tn" +%w`
+  day_of_week=$(date --date="$tn" +%w)
   echo "day_of_week: $day_of_week"
 fi
 
 # The night sign is four ahead of the day sign
 let "night_of_week_p=day_of_week+4"
-night_of_week=`expr $night_of_week_p % 7`
+night_of_week=$(expr $night_of_week_p % 7)
 
-Sh=`date --date="$S" +%H` # Sunsetet hour
-Sm=`date --date="$S" +%M` # Sunset minute
-tfR=`date --date="$tn -$Rh hours -$Rm minutes" +%H:%M` # Time from Rise
-tfS=`date --date="$DS -$tnh hours -$tnm minutes" +%H:%M` # Time from set
+Sh=$(date --date="$S" +%H) # Sunsetet hour
+Sm=$(date --date="$S" +%M) # Sunset minute
+tfR=$(date --date="$tn -$Rh hours -$Rm minutes" +%H:%M) # Time from Rise
+tfS=$(date --date="$DS -$tnh hours -$tnm minutes" +%H:%M) # Time from set
 
 
 echo "Rise/Set: $R, $S"
@@ -72,14 +72,14 @@ let "DRS_t=DRS_h+1"
 
 for (( i=0; i<$DRS_t; i++ ))
 do 
-  DAY+=("`date --date="$DR +$i hour" +%Y-%m-%d\ %R\ %Z`")
+  DAY+=("$(date --date="$DR +$i hour" +%Y-%m-%d\ %R\ %Z)")
 done 
 
 # NIGHT array
 let "DRR_t=24-DRS_h"
 for (( i=0; i<$DRR_t; i++ ))
 do 
-  NIGHT+=("`date --date="$DS +$i hour" +%Y-%m-%d\ %R\ %Z`")
+  NIGHT+=("$(date --date="$DS +$i hour" +%Y-%m-%d\ %R\ %Z)")
 done
 
 # Is the current DAY/NIGHT hour member the curretn hour?
@@ -90,10 +90,10 @@ function is_current (){ # Pass the index, the DAY/NIGHT array
   ARR="$2"
   NEXT_HOUR_ev="date --date=\"\${$ARR[$next_hour]}\" +%R"
   HOUR_ev="date --date=\"\${$ARR[$i]}\" +%R"
-  NEXT_HOUR=`eval $NEXT_HOUR_ev`
-  HOUR=`eval $HOUR_ev`
-  COMP=`comp_time $tnh:$tnm $HOUR`
-  COMP_NEXT=`comp_time $tnh:$tnm $NEXT_HOUR`
+  NEXT_HOUR=$(eval $NEXT_HOUR_ev)
+  HOUR=$(eval $HOUR_ev)
+  COMP=$(comp_time $tnh:$tnm $HOUR)
+  COMP_NEXT=$(comp_time $tnh:$tnm $NEXT_HOUR)
   if [[ $COMP -eq 1 && $COMP_NEXT -eq 0 || -z $COMP_NEXT ]];
   then
     cur_l="\e[33m"
@@ -114,13 +114,13 @@ echo "DAY:"
   cur_planet_i=${hplanets_i[$start_day]} # Get index to start in hplanets
 for i in ${!DAY[@]}
 do 
-  P_DAY=`is_current $i "DAY"`
+  P_DAY=$(is_current $i "DAY")
   let "planet=i+cur_planet_i"
-  dplanet=`expr $planet % 7`
+  dplanet=$(expr $planet % 7)
   cur_planet=${hplanets[$dplanet]}
   echo -e $P_DAY ${planets[$cur_planet]} 
   let "start_day++"
-  start_day=`expr $start_day % 7`
+  start_day=$(expr $start_day % 7)
 done
 
 echo -e ""
@@ -129,11 +129,11 @@ echo "NIGHT:"
   cur_planet_i=${hplanets_i[$start_night]} # Get an index where to start hplanets
 for i in ${!NIGHT[@]}
 do
-  P_NIGHT=`is_current $i "NIGHT"`
+  P_NIGHT=$(is_current $i "NIGHT")
   let "planet=i+cur_planet_i"
-  dplanet=`expr $planet % 7`
+  dplanet=$(expr $planet % 7)
   cur_planet=${hplanets[$dplanet]}
   echo -e $P_NIGHT ${planets[$cur_planet]}
   let "start_night++"
-  start_night=`expr $start_night % 7`
+  start_night=$(expr $start_night % 7)
 done 
